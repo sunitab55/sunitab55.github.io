60682447b2608c27889cf5a41f3afe1df186568b
souvik1997 <souvik1997@users.noreply.github.com>
2021-08-13 03:34:24 +0000

    deploy: 4dbdc272e4c502036c5a5fd65429fbf60f58e4f7

---
+++ _next/static/idkDiQezJushNbjIDwxCP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/732-37d12a1f97caab50a4af.js","static/css/b45dc3ec539c3fd4a344.css","static/chunks/pages/index-c74397061c319c4a9f9a.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
