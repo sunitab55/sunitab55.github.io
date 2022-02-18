73d4995c0de041aae66869b3915af7a64123920d
souvik1997 <souvik1997@users.noreply.github.com>
2022-02-18 01:57:56 +0000

    deploy: 09d17c0e857cab4211861d084f40cdbabc74286c

--- _next/static/yJGHbAr1jyYuc9dcJ7txx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/732-37d12a1f97caab50a4af.js","static/css/b45dc3ec539c3fd4a344.css","static/chunks/pages/index-89f8856fc0705dcb2ef8.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
